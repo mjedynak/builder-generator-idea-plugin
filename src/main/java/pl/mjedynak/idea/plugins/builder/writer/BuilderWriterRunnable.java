35423e45eb963db7161140271b384066fb7407a1
Michal Jedynak <m.jedynak@gmail.com>
2011-11-27 21:34:27 +0100

    * refactoring

--- src/main/java/pl/mjedynak/idea/plugins/builder/writer/BuilderWriterRunnable.java
+++ src/main/java/pl/mjedynak/idea/plugins/builder/writer/BuilderWriterRunnable.java
@@ -1,18 +1,10 @@
 package pl.mjedynak.idea.plugins.builder.writer;
 
-import com.intellij.codeInsight.CodeInsightBundle;
-import com.intellij.codeInsight.CodeInsightUtil;
 import com.intellij.codeInsight.generation.PsiElementClassMember;
 import com.intellij.openapi.application.Application;
-import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.fileEditor.ex.IdeDocumentHistory;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.ui.Messages;
-import com.intellij.openapi.util.Computable;
 import com.intellij.psi.PsiClass;
 import com.intellij.psi.PsiDirectory;
-import com.intellij.psi.PsiElement;
-import com.intellij.util.IncorrectOperationException;
 import pl.mjedynak.idea.plugins.builder.psi.BuilderPsiClassBuilder;
 import pl.mjedynak.idea.plugins.builder.psi.PsiHelper;
 
@@ -42,42 +34,6 @@ public class BuilderWriterRunnable implements Runnable {
     @Override
     public void run() {
         Application application = psiHelper.getApplication();
-        application.runWriteAction(new Computable<PsiElement>() {
-            public PsiElement compute() {
-                return createBuilder(project, classMembers, targetDirectory, className, psiClassFromEditor);
-            }
-        });
-    }
-
-    private PsiElement createBuilder(final Project project, List<PsiElementClassMember> classMembers, PsiDirectory targetDirectory, final String className, PsiClass psiClassFromEditor) {
-        try {
-            IdeDocumentHistory.getInstance(project).includeCurrentPlaceAsChangePlace();
-            PsiClass targetClass = getBuilderPsiClass(project, classMembers, targetDirectory, className, psiClassFromEditor);
-            navigateToClassAndPositionCursor(project, targetClass);
-            return targetClass;
-        } catch (IncorrectOperationException e) {
-            showErrorMessage(project, className);
-            e.printStackTrace();
-            return null;
-        }
-    }
-
-    private PsiClass getBuilderPsiClass(Project project, List<PsiElementClassMember> classMembers, PsiDirectory targetDirectory, String className, PsiClass psiClassFromEditor) {
-        return builderPsiClassBuilder.aBuilder(project, targetDirectory, psiClassFromEditor, className, classMembers)
-                .withFields().withPrivateConstructor().withInitializingMethod().withSetMethods().build();
-    }
-
-    private void navigateToClassAndPositionCursor(Project project, PsiClass targetClass) {
-        CodeInsightUtil.positionCursor(project, targetClass.getContainingFile(), targetClass.getLBrace());
-    }
-
-    private void showErrorMessage(final Project project, final String className) {
-        ApplicationManager.getApplication().invokeLater(new Runnable() {
-            public void run() {
-                Messages.showErrorDialog(project,
-                        CodeInsightBundle.message("intention.error.cannot.create.class.message", className),
-                        CodeInsightBundle.message("intention.error.cannot.create.class.title"));
-            }
-        });
+        application.runWriteAction(new BuilderWriterComputable(builderPsiClassBuilder, project, classMembers, targetDirectory, className, psiClassFromEditor, psiHelper));
     }
 }
--- src/test/java/pl/mjedynak/idea/plugins/builder/writer/BuilderWriterRunnableTest.java
+++ src/test/java/pl/mjedynak/idea/plugins/builder/writer/BuilderWriterRunnableTest.java
@@ -1,17 +1,27 @@
 package pl.mjedynak.idea.plugins.builder.writer;
 
 import com.intellij.codeInsight.generation.PsiElementClassMember;
+import com.intellij.openapi.application.Application;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiClass;
 import com.intellij.psi.PsiDirectory;
 import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 import pl.mjedynak.idea.plugins.builder.psi.BuilderPsiClassBuilder;
 import pl.mjedynak.idea.plugins.builder.psi.PsiHelper;
 
 import java.util.Arrays;
 import java.util.List;
 
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+@RunWith(MockitoJUnitRunner.class)
 public class BuilderWriterRunnableTest {
 
     private BuilderWriterRunnable builderWriterRunnable;
@@ -42,6 +52,19 @@ public class BuilderWriterRunnableTest {
     public void setUp() {
         psiElementClassMembers = Arrays.asList(psiElementClassMember);
         builderWriterRunnable = new BuilderWriterRunnable(builderPsiClassBuilder, project, psiElementClassMembers, targetDirectory, builderClassName, srcClass, psiHelper);
+    }
+
+    @Test
+    public void shouldRunWriteActionWithBuilderWriterComputable() {
+        // given
+        Application application = mock(Application.class);
+        given(psiHelper.getApplication()).willReturn(application);
+
+        // when
+        builderWriterRunnable.run();
+
+        // then
+        verify(application).runWriteAction(any(BuilderWriterComputable.class));
     }
 
 
