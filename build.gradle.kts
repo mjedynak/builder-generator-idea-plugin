3630aafd9363b858c558e6f3f6c2b5d1e4d8cb0d
mjedynak <m.jedynak@gmail.com>
2022-07-14 23:56:45 +0200

    update github actions

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,8 +10,8 @@ repositories {
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
 
     testImplementation("org.mockito:mockito-junit-jupiter:4.6.1")
     testImplementation("org.assertj:assertj-core:3.23.1")
